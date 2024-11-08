8616615ce81062081e6d8fb9d37b0f81539a7a1a
HrushikeshPawar <hrushikesh.vpawar@gmail.com>
2024-11-08 16:26:02 +0530

    :sparkles:feat: Add notebook for adv rag using llamaindex in mlflow

---
+++ mlflow/llamaindex-adv-rag-model.py
@@ -0,0 +1,79 @@
+import mlflow
+
+from pathlib import Path
+from llama_index.llms.gemini import Gemini
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Document, StorageContext, load_index_from_storage, ServiceContext
+from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.indices.postprocessor import MetadataReplacementPostProcessor, SentenceTransformerRerank
+from llama_index.core.response.notebook_utils import display_response
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Defining the CONSTANTS
+DATA_DIR = Path('..', 'data')
+INPUT_FPATH = Path(DATA_DIR, "eBook-How-to-Build-a-Career-in-AI.pdf")
+SENTENCE_INDEX_PATH = Path(DATA_DIR, "sentence_index")
+
+LLM_MODEL_NAME = "models/gemini-1.5-flash-002"
+TEMPERATURE = 0.1
+
+EMBED_MODEL = "local:BAAI/bge-small-en-v1.5" # "local:BAAI/bge-large-en-v1.5"
+RERANK_MODEL = "BAAI/bge-reranker-base"
+
+SENTENCE_WINDOW_SIZE = 3
+SIMILARITY_TOP_K = 6
+RERANK_TOP_N = 2
+
+# Setting up LLM
+llm = Gemini(model_name=LLM_MODEL_NAME, temperature=TEMPERATURE)
+
+# Index Creation
+## Load data
+documents = SimpleDirectoryReader(input_files=[INPUT_FPATH]).load_data()
+
+## Convert into a Document
+document = Document(text="\n\n".join([doc.text for doc in documents]))
+
+
+# Creating Sentence Index
+## Create the sentence window node parser w/ default settings
+node_parser = SentenceWindowNodeParser.from_defaults(
+    window_size=SENTENCE_WINDOW_SIZE,
+    window_metadata_key="window",
+    original_text_metadata_key="original_text",
+)
+
+## Sentence Context Model
+sentence_context = ServiceContext.from_defaults(
+    llm=llm,
+    embed_model=EMBED_MODEL,
+    node_parser=node_parser,
+)
+
+# Create the sentence index
+# if an index file exist, then it will load it
+# if not, it will rebuild it
+if not SENTENCE_INDEX_PATH.exists():
+    sentence_index = VectorStoreIndex.from_documents([document], service_context=sentence_context)
+    sentence_index.storage_context.persist(persist_dir=SENTENCE_INDEX_PATH)
+
+else:
+    sentence_index = load_index_from_storage(StorageContext.from_defaults(persist_dir=SENTENCE_INDEX_PATH), service_context=sentence_context)
+
+# Post Processing
+## Instead of passing only the retrieved sentence, we pass a window of sentences - Sentence Window Retrieval
+postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
+
+## Rerank the sentences using a Sentence Transformer
+rerank = SentenceTransformerRerank(top_n=RERANK_TOP_N, model=RERANK_MODEL)
+
+# Create the query engine
+sentence_window_engine = sentence_index.as_query_engine(similarity_top_k=SIMILARITY_TOP_K, node_postprocessors=[postproc, rerank])
+
+# Setting up MLFlow
+mlflow.models.set_model(sentence_window_engine)
